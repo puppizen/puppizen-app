dd461eadbb5f491a7b6ecd1f7db0120862d69e38
puppizen <puppizen@gmail.com>
2025-09-22 02:55:00 +0100

    another commit

--- app/api/bots/route.ts
+++ app/api/bots/route.ts
@@ -50,7 +50,7 @@ export async function POST(req: NextRequest) {
   } 
 
   const userId = update?.message?.from?.id;
-  const username = update?.message?.from?.username ?? 'Anonymous';
+  const username = update?.message?.from?.username ?? `user-${userId}`;
   const messageText = update?.message?.text;
   const chatId = update?.message?.chat?.id;
   
