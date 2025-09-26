bd92ee57510d6a9a9ea01aed3a053a4d7b52c60a
puppizen <puppizen@gmail.com>
2025-09-26 17:17:29 +0100

    new fin commit

--- app/components/SocialTasks.tsx
+++ app/components/SocialTasks.tsx
@@ -118,7 +118,7 @@ export default function PartnersTasks({ category }: { category: string }) {
           className="flex justify-between items-center mb-3.5 my-bg-lightgray px-2 py-5 rounded-md"
         >
           <div className="flex justify-center gap-3">
-              <Image src={task.iconUrl} width={26} height={26} alt="" className="my-bg-gray p-1"/>
+              <Image src={task.iconUrl} width={26} height={26} alt="" />
             <div>
               <p className="text-sm">{task.name}</p>
               <p className="flex items-center gap-3 mt-0.5">  
