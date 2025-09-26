8e6795f3c7cd569eab10ac2d9a4959fbd8d8f9a5
puppizen <puppizen@gmail.com>
2025-09-26 16:41:33 +0100

    final commit for now

--- app/components/UserTGData.tsx
+++ app/components/UserTGData.tsx
@@ -59,8 +59,8 @@ export default function UserTGData() {
 
           <div className="absolute left-0 top-0">
             <span className="relative flex size-3">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
-              <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"></span>
+              <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
             </span>
           </div>
         </div>
