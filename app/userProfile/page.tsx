766c87d5c9020bfba34fc07655bf5c6eeb5eb4d3
puppizen <puppizen@gmail.com>
2025-09-26 13:19:16 +0100

    another commit

--- app/userProfile/page.tsx
+++ app/userProfile/page.tsx
@@ -16,15 +16,15 @@ type UserData = {
 }
 
 export default function UserProfile() {
-  const [userData, setUserData] = useState<UserData[]>([]);
-  const [cachedData, setCachedData] = useState<UserData[]>([]);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [cachedData, setCachedData] = useState<UserData | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const tgUser = window.Telegram.WebApp.initDataUnsafe?.user;
 
     setLoading(true);
-    setCachedData([]);
+    setCachedData(null);
 
     const cacheKey = `cachedData-${tgUser.id}`
 
@@ -42,7 +42,7 @@ export default function UserProfile() {
       fetch(`/api/userData?userId=${tgUser.id}`)
       .then((res) => res.json())
       .then((data) => {
-        setUserData(data)
+        setUserData(data);
         localStorage.setItem(cacheKey, JSON.stringify(data));
         setLoading(false);
       })
@@ -55,34 +55,36 @@ export default function UserProfile() {
     <div className="flex flex-col justify-around">
       <h3 className="my-text-gray font-bold text-2xl text-center">My Profile</h3>
       <div>
-        {displayData.map((userData) => (
-          <div key={userData.userId} className="flex flex-col justify-around">
+        {displayData ? (
+          <div className="flex flex-col justify-around">
             <div>
-              <Image src={userData.profile_url || '/puppizen-image.png'} width={50} height={50} alt="user photo" className="my-border-gray"/>
-              <p className="text-center mt-5">{userData.username}</p>
+              <Image src={displayData.profile_url || '/puppizen-image.png'} width={50} height={50} alt="user photo" className="my-border-gray"/>
+              <p className="text-center mt-5">{displayData.username}</p>
             </div>
             <div className="flex flex-col justify-around">
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>$PUPPIZEN</span> <span>{userData.balance}</span></p>
+                <p className="flex justify-between items-center"><span>$PUPPIZEN</span> <span>{displayData.balance}</span></p>
               </div>
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>$STARS</span> <span>{userData.starsBalance}</span></p>
+                <p className="flex justify-between items-center"><span>$STARS</span> <span>{displayData.starsBalance}</span></p>
               </div>
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>$STARS spent</span> <span>{userData.totalStarsPaid}</span></p>
+                <p className="flex justify-between items-center"><span>$STARS spent</span> <span>{displayData.totalStarsPaid}</span></p>
               </div>
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>Referrals</span> <span>{userData.referrals}</span></p>
+                <p className="flex justify-between items-center"><span>Referrals</span> <span>{displayData.referrals}</span></p>
               </div>
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>Task Completed</span> <span>{userData.taskCompleted}</span></p>
+                <p className="flex justify-between items-center"><span>Task Completed</span> <span>{displayData.taskCompleted}</span></p>
               </div>
               <div className="my-bg-gray my-border-white p-3 rounded-md">
-                <p className="flex justify-between items-center"><span>Ads Watched</span> <span>{userData.adsWatched}</span></p>
+                <p className="flex justify-between items-center"><span>Ads Watched</span> <span>{displayData.adsWatched}</span></p>
               </div>
             </div>
           </div>
-        ))}
+        ) : (
+          <p></p>
+        )}
       </div>
     </div>
   )
