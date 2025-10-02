ffc00a3459309ae1498418d502f6cab28db8c795
puppizen <puppizen@gmail.com>
2025-10-02 19:14:44 +0100

    another commit

--- app/components/Games.tsx
+++ app/components/Games.tsx
@@ -1,8 +1,8 @@
 export default function Games() {
   return (
-    <div className="bg-yellow-500 w-full py-5 flex flex-col gap-3 text-center rounded-md">
-      <h3 className="my-text-black">Games</h3>
-      <p className="my-text-black">Comming Soon!</p>
+    <div className="bg-yellow-500 w-full py-5 flex flex-col gap-1 text-center rounded-md">
+      <h3 className="my-text-black font-medium text-lg">Games</h3>
+      <p className="my-text-black my-bg-dark p-3 rounded-full">Coming Soon!</p>
     </div>
   )
 }
