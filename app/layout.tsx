467a3e168addc066681a0dc512ee9453281debbb
puppizen <puppizen@gmail.com>
2025-10-04 19:40:59 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
 
         <Script src="https://sad.adsgram.ai/js/sad.min.js"></Script>
