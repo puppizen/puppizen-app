7fd0d8204e2d5c73e81c5aeb852794db1f8c4499
puppizen <puppizen@gmail.com>
2025-10-05 18:44:35 +0100

    mirror image

--- app/components/RewardBooster.tsx
+++ app/components/RewardBooster.tsx
@@ -3,16 +3,16 @@ import Image from "next/image"
 export default function RewardBooster() {
   return (
     <div className="my-bg-image bg-amber-400 rounded-md">
-      <div className="relative overflow-hidden flex justify-between items-center px-3 py-8">
+      <div className="relative overflow-hidden flex justify-between items-center px-3 py-5">
         <div className="relative z-10">
-          <h2 className="text-lg flex flex-col">
+          <h2 className="text-lg font-bold flex flex-col">
             <span>BOOST YOUR</span>
             <span>REWARDS</span>
           </h2>
         </div>
         
         
-        <Image className="absolute right-10 top-1 object-contain" src={"/rewardBooster.png"} width={150} height={150} alt="boost"/>
+        <Image className="absolute right-5 top-1 object-contain scale-x-[-1]" src={"/rewardBooster.png"} width={150} height={150} alt="boost"/>
       </div>
     </div>
   )
