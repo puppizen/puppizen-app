92fe205bd79f261b4c5c613b223e98a5990a2b56
puppizen <puppizen@gmail.com>
2025-10-05 18:51:17 +0100

    another commit

--- app/components/RewardBooster.tsx
+++ app/components/RewardBooster.tsx
@@ -7,12 +7,12 @@ export default function RewardBooster() {
         <div className="relative z-10">
           <h2 className="text-lg font-bold flex flex-col">
             <span>BOOST YOUR</span>
-            <span>REWARDS</span>
+            <span className="my-bg-dark my-text-white p-3 rounded-full">REWARDS x2</span>
           </h2>
         </div>
         
         
-        <Image className="absolute right-5 top-1 object-contain scale-x-[-1]" src={"/rewardBooster.png"} width={150} height={150} alt="boost"/>
+        <Image className="absolute right-5 -top-1 object-contain scale-x-[-1]" src={"/rewardBooster.png"} width={160} height={150} alt="boost"/>
       </div>
     </div>
   )
