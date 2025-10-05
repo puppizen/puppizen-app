cfb49dd6d5711a3d6de595e2c7a01ba7b9b4b45a
puppizen <puppizen@gmail.com>
2025-10-05 18:34:49 +0100

    another commit

--- app/components/RewardBooster.tsx
+++ app/components/RewardBooster.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function RewardBooster() {
   return (
     <div className="my-bg-image bg-amber-400 rounded-md">
-      <div className="relative overflow-hidden flex justify-between items-center px-3 py-5">
+      <div className="relative overflow-hidden flex justify-between items-center px-3 py-8">
         <div className="relative z-10">
           <h2 className="text-lg flex flex-col">
             <span>BOOST YOUR</span>
@@ -12,7 +12,7 @@ export default function RewardBooster() {
         </div>
         
         
-        <Image className="absolute right-0 top-3 object-contain" src={"/rewardBooster.png"} width={150} height={150} alt="boost"/>
+        <Image className="absolute right-10 top-1 object-contain" src={"/rewardBooster.png"} width={150} height={150} alt="boost"/>
       </div>
     </div>
   )
