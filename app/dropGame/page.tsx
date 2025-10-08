d3a25c2ddb26d81a3a937d51ce6fcdd3b27dbe64
puppizen <puppizen@gmail.com>
2025-10-08 14:08:32 +0100

    another commit

--- app/dropGame/page.tsx
+++ app/dropGame/page.tsx
@@ -4,8 +4,8 @@ import DropGameCanvas from "../components/DropGameCanvas"
 
 export default function DropGame() {
   return (
-    <div className="relative w-full h-full overflow-hidden">
-      <svg className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-[200vh] h-[200vw] z-0 pointer-events-none bg-gradient-to-b from-transparent via-lime-500 to-black opacity-20">
+    <div className="p-0 relative w-full h-full overflow-hidden">
+      <svg className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-[200vh] h-[200vw] z-0 pointer-events-none bg-gradient-to-b from-transparent via-lime-900 to-black opacity-20">
         <defs>
           <pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse">
             <path d="M 80 0 L 0 0 0 80" fill="none" stroke="lime" strokeWidth="0.5" />
