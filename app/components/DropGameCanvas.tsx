0229f34532cbf9262b2fb6ac7c3c61d67c457111
puppizen <puppizen@gmail.com>
2025-10-09 13:15:57 +0100

    another commit

--- app/components/DropGameCanvas.tsx
+++ app/components/DropGameCanvas.tsx
@@ -193,16 +193,16 @@ export default function DropGameCanvas() {
       
 
       {gameOver && (
-        <div className="absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/5 flex flex-col gap-3">
+        <div className="absolute top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 flex flex-col gap-3">
           <div className="text-center p-3 bg-black/40 backdrop-blur-md rounded-md w-full">
-            <p className="font-light">Your Score</p>
-            <p className="text-6xl font-bold mt-2">{score}</p>
+            <p className="font-[100] text-amber-400">Your Score</p>
+            <p className="text-6xl font-bold mt-2 text-amber-400">{score}</p>
             <p className="text-lg font-bold mt-5">{getEndMessage(score).head}</p>
             <p className="font-[200] text-sm my-text-gray mt-1">{getEndMessage(score).text}</p>
           </div> 
           <div className="w-full flex flex-col gap-2">
-            <button className="my-bg-gradient py-1 rounded-full w-full">Claim</button>  
-            <button className="w-full py-1 my-bg-white rounded-full">Play again</button>
+            <button className="bg-amber-400/40 py-1 rounded-full w-full">Claim</button>  
+            <button className="w-full py-2 bg-white/40 text-amber-400 rounded-full">Play again</button>
           </div>      
         </div>
       )}
