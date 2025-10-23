e818031ef7dfe5e4fc45a30710221518357bbf8e
puppizen <puppizen@gmail.com>
2025-10-23 15:08:49 +0100

    another commit

--- app/components/TelegramInit.tsx
+++ app/components/TelegramInit.tsx
@@ -1,44 +1,35 @@
-'use client'
-import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
+'use client';
+import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 
-export default function TelegramInit(){
+export default function TelegramInit() {
   const router = useRouter();
   const pathname = usePathname();
 
-   useEffect(() => {
+  useEffect(() => {
     const tg = window.Telegram?.WebApp;
     if (!tg) return;
 
     tg.ready();
     tg.expand();
 
-    if (pathname === "/" || pathname === "/home") {
+    if (pathname === '/' || pathname === '/home') {
+      // Show Close button on home page
       tg.BackButton.hide();
       tg.CloseButton.show();
     } else {
-      tg.CloseButton.hide()
-      tg.BackButton.onClick(() => {
-        router.back() 
-      });
+      // Show Back button on other pages
+      tg.CloseButton.hide();
       tg.BackButton.show();
+      tg.BackButton.onClick(() => router.back());
     }
-    
 
     return () => {
-      tg.BackButton.hide();
       tg.BackButton.offClick();
+      tg.BackButton.hide();
+      tg.CloseButton.hide();
     };
-
-    // const root = document.documentElement;
-    // root.style.setProperty('--tg-bg-color', tg.themeParams?.bg_color || '#000000');
-    // root.style.setProperty('--tg-text-color', tg.themeParams?.text_color || '#ffffff');
-    // root.style.setProperty('--tg-button-color', tg.themeParams?.button_color || '#00bfa5');
-    // root.style.setProperty('--tg-button-text-color', tg.themeParams?.button_text_color || '#ffffff');
   }, [pathname, router]);
 
-  return (
-    <>
-    </>
-  );
+  return null;
 }
