a13a55f4ada3eec380b138b0eacf8f8913c0785b
puppizen <puppizen@gmail.com>
2025-10-24 12:40:06 +0100

    add analytics identifier

--- app/components/TelegramInit.tsx
+++ app/components/TelegramInit.tsx
@@ -32,7 +32,7 @@ export default function TelegramInit() {
   useEffect(() => {
     TelegramAnalytics.init({
         token: 'eyJhcHBfbmFtZSI6InB1cHBpemVuIiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9QdXBwaXplbkJvdCIsImFwcF9kb21haW4iOiJodHRwczovL3B1cHBpemVuLWFwcC52ZXJjZWwuYXBwLyJ9!USCqBE5HvhCDrqFw7BtISkeaRLWhTzUn1mGTywNIwj0=',
-        appName: 'Puppizen',
+        appName: 'puppizen',
     });
   }, [])
 
