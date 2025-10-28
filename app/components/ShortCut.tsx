836c727bd720a44c142a06a993e2f26845b7d3ae
puppizen <puppizen@gmail.com>
2025-10-28 15:21:08 +0100

    another commit

--- app/components/ShortCut.tsx
+++ app/components/ShortCut.tsx
@@ -4,40 +4,40 @@ import Image from "next/image";
 export default function ShortCut() {
   return (
     <div className="grid grid-cols-4 w-full">
-      <Link href='/leaderboard'  className='flex flex-row justify-between btn-translate-active items-center'>
-        <div className='flex flex-row items-center gap-2'>
-          <div className="bg-gray-600/50 rounded-full p-3">
-            <Image src='/trophy.svg' width={20} height={20} alt=''></Image>
+      <Link href='/leaderboard'>
+        <div className='flex flex-col items-center gap-2'>
+          <div className="bg-gray-600/40 rounded-full p-3">
+            <Image src='/trophy.svg' width={24} height={24} alt=''></Image>
           </div>
           
           <span className='text-xs font-thin'>Leaderboard</span>
         </div>
       </Link>
 
-      <Link href='/presale'  className='flex flex-row justify-between btn-translate-active items-center'>
-        <div className='flex flex-row items-center gap-2'>
-          <div className="bg-gray-600/50 rounded-full p-3">
-            <Image src='/presale.svg' width={20} height={20} alt=''></Image>
+      <Link href='/presale'>
+        <div className='flex flex-col items-center gap-2'>
+          <div className="bg-gray-600/40 rounded-full p-3">
+            <Image src='/presale.svg' width={24} height={24} alt=''></Image>
           </div>
           
           <span className='text-xs font-thin'>Presale</span>
         </div>
       </Link>
 
-      <Link href='/userProfile'  className='flex flex-row justify-between btn-translate-active items-center'>
-        <div className='flex flex-row items-center gap-2'>
-          <div className="bg-gray-600/50 rounded-full p-3">
-            <Image src='/profile.svg' width={20} height={20} alt=''></Image>
+      <Link href='/userProfile'>
+        <div className='flex flex-col items-center gap-2'>
+          <div className="bg-gray-600/40 rounded-full p-3">
+            <Image src='/profile.svg' width={24} height={24} alt=''></Image>
           </div>
           
           <span className='text-xs font-thin'>Profile</span>
         </div>
       </Link>
 
-      <Link href='/booster'  className='flex flex-row justify-between btn-translate-active items-center'>
-        <div className='flex flex-row items-center gap-2'>
-          <div className="bg-gray-600/50 rounded-full p-3">
-            <Image src='/boostImage.png' width={20} height={20} alt=''></Image>
+      <Link href='/booster'>
+        <div className='flex flex-col items-center gap-2'>
+          <div className="bg-gray-600/40 rounded-full p-3">
+            <Image src='/boostImage.png' width={24} height={24} alt=''></Image>
           </div>
           
           <span className='text-xs font-thin'>Booster</span>
